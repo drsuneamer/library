be2fd1c12b940e5ce2604eb8451a687eb337c6c1
Sunyeong Shin <sunyeong0412@gmail.com>
2024-09-24 00:37:23 +0000

    readmodel

--- bookdetail/src/main/java/library/domain/Bookdetail.java
+++ bookdetail/src/main/java/library/domain/Bookdetail.java
@@ -13,7 +13,7 @@ import lombok.Data;
 public class Bookdetail {
 
     @Id
-    //@GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     private Long bookId;
